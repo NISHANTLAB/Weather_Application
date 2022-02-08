551cecfe75de493a008019970433ca6afdd23d41
NISHANTLAB <nishantkumarsingh355@gmail.com>
2022-02-08 17:05:04 +0530

    latest temp data

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "ea01cdf8296712b2bb95ce664ce86c84"
+    }
+}
+
+module.exports = constants;
